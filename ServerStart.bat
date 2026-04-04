5754a0e619c85b745c16c2b6690f965595571c84
Enos-Cloud <enoslo1130@gmail.com>
2026-04-04 11:23:25 +0800

    Prefer Java 21 in launcher

--- ServerStart.bat
+++ ServerStart.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-setlocal EnableExtensions
+setlocal EnableExtensions EnableDelayedExpansion
 chcp 65001 >nul
 COLOR 17
 
@@ -20,12 +20,30 @@ goto begin
 :begin
 title 880TEST - LinServer880 伺服器啟動
 
+set "JAVA_CMD=java"
+for /f "delims=" %%J in ('where java 2^>nul') do (
+	for /f "tokens=3" %%V in ('"%%J" -version 2^>^&1 ^| findstr /i /c:"version"') do (
+		set "JAVA_VERSION=%%~V"
+		if "!JAVA_VERSION:~0,2!"=="21" (
+			set "JAVA_CMD=%%J"
+			goto java_found
+		)
+	)
+)
+
+:java_found
+if /i not "%JAVA_CMD%"=="java" (
+	echo 偵測到 Java 21：[%JAVA_CMD%] [將直接使用此環境啟動]
+) else (
+	echo 未找到 Java 21，將使用預設 java 指令 [未找到 Java 21，將使用預設 java]
+)
+
 set "L1J_RAM=-server -Xms16384m -Xmx16384m -Xmn6144m -XX:MaxGCPauseMillis=50"
 set "L1J_TIME=-Duser.timezone=Asia/Taipei"
 set "L1J_PATH=-cp Server_Game.jar;jar\commons-logging-1.1.1.jar;jar\javolution-5.5.1.jar;jar\slf4j-reload4j-2.0.3.jar;jar\reload4j-1.2.26.jar;jar\mariadb-java-client-3.0.6.jar;jar\HikariCP-4.0.3.jar;jar\slf4j-api-2.0.3.jar;jar\netty-buffer-4.1.131.Final.jar;jar\netty-codec-4.1.131.Final.jar;jar\netty-common-4.1.131.Final.jar;jar\netty-handler-4.1.131.Final.jar;jar\netty-resolver-4.1.131.Final.jar;jar\netty-transport-4.1.131.Final.jar;jar\protobuf-3.21.7.jar;tools\quest_recover_classes com.lineage.Server"
 
 echo Starting server... [伺服器啟動中]
-java %L1J_RAM% %L1J_TIME% %L1J_PATH%
+"%JAVA_CMD%" %L1J_RAM% %L1J_TIME% %L1J_PATH%
 
 cls
 goto check_update
